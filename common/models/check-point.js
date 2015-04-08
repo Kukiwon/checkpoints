26644def703528ec37e1893ec1dc0b1a959135bd
Jordy van Kuijk <jvankuijk@me.com>
2015-04-08 22:43:07 +0200

    added recursive delete for project and checkpoint route

--- common/models/check-point.js
+++ common/models/check-point.js
@@ -1,3 +1,19 @@
 module.exports = function(CheckPoint) {
-
+	CheckPoint.observe('before delete', function init(ctx, next) {
+		ctx.Model.findOne({
+				where: {
+					id: ctx.where.id,
+				},
+				include: ['sessionCheckPoints']
+			} , function(e, checkPoint) {
+			if(!!!checkPoint || e) {
+				next();
+				return;
+			}
+			if(checkPoint.sessionCheckPoints && checkPoint.sessionCheckPoints.length) {
+				checkPoint.sessionCheckPoints.destroyAll();
+			}
+			next();
+		});
+	});
 };
--- common/models/project.js
+++ common/models/project.js
@@ -1,7 +1,7 @@
 var crypto = require('crypto'),
 biguint = require('biguint-format');
 
-module.exports = function(Project) {
+module.exports = function(Project, CheckPoint) {
 
 	Project.observe('before save', function init(ctx, next) {
 		if(typeof ctx.instance.created === 'undefined')
@@ -9,5 +9,34 @@ module.exports = function(Project) {
 		if(typeof ctx.instance.hash === 'undefined')
 			ctx.instance.hash = biguint(crypto.randomBytes(10), 'dec');
 		next();
+	});
+
+	Project.observe('before delete', function init(ctx, next) {
+		ctx.Model.findOne({
+				where: {
+					id: ctx.where.id,
+				},
+				include: [{'sessions': ['sessionCheckPoints']}, 'checkPoints']
+			} , function(e, project) {
+			if(!!!project || e) {
+				next();
+				return;
+			}
+			if(project.checkPoints && project.checkPoints.length) {
+				project.checkPoints.destroyAll();
+			}
+
+			if(project.sessions && project.sessions.length) {
+				project.__get__sessions(function(e, sessions) {
+					if(sessions && sessions.length) {
+						sessions.forEach(function(session) {
+							session.sessionCheckPoints.destroyAll();
+						});
+					}
+					project.sessions.destroyAll();
+				});
+			}
+			next();
+		});
 	});
 };
